f9e79e855289bb39f2885dc93d9c43c6131a07a2
Nick Garbalau <garbalaunick@gmail.com>
2023-03-31 17:29:14 +0300

    Add Types

---
+++ types.d.ts
@@ -0,0 +1,16 @@
+export type MessageType = {
+  datetime: string;
+  messageId: string;
+  text: string;
+  userId: string;
+};
+
+export type UserType = {
+  value: string;
+  label: string;
+};
+
+export type ChannelType = {
+  value: string;
+  label: string;
+};
